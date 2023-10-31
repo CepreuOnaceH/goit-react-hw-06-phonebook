46396bfaa7f4f959d36155cbd892c9aba58db15c
Sergey Opasen <ykcyc13@gmail.com>
2023-10-31 14:46:00 +0100

    update filtersSlice

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setFilter } from 'redux/contactsSlice';
+import { setFilter } from 'redux/filtersSlice';
 
 function Filter() {
   const filter = useSelector(state => state.filter);
